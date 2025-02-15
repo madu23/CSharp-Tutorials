a0dcea95ce2eec07af0d9b9f65a32731b371272e
Eke Oriaku <ekeby2k@yahoo.co.uk>
2025-02-15 12:30:03 +0000

    refactored code to support modular development

--- Restaurant.Application/Program.cs
+++ Restaurant.Application/Program.cs
@@ -1,50 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+using Restaurant.Application.Services;
 using Restaurant.Domain.Db;
 using Restaurant.Domain.Entities;
 
 namespace Restaurant.Application;
 class Program
 {
-    static void Main(string[] args)
+    //static void Main(string[] args)
+    //{
+
+    //}
+    static async Task Main(string[] args)
     {
         Console.WriteLine("Hello, Welcome to Eke Tech Restaurant!");
 
-        // when application starts up, verify you have an admin account already setup.
-        var adminStaff = AppDb.StaffTable.Where(_ => _.Value.Designation == "System Admin").FirstOrDefault();
-        if(adminStaff.Value == null)
+        // seed default admin data step 1
+        var seedDbTask = new StartupTask();
+       var result =  await seedDbTask.SeedAdminRecord();
+        if(result == false)
         {
-            // meaning there is no admin. So go ahead and create one
-            var newAdminStaff = new Staff
-            {
-                StaffId = 1,
-                FirstName = "System",
-                LastName = "Admin",
-                Designation = "System Admin",
-                Password = "superPassword"
-            };
-
-            newAdminStaff.CreateStaff();
+            Console.WriteLine("Admin data was not successfully pre-created");
         }
 
-        Console.WriteLine("Enter your login details");
-        Console.WriteLine("Staff Id:");
-        var staffId = Convert.ToInt16(Console.ReadLine());
+        // Login Step 2
+        var authService = new AuthService();
+        var loginResult = await authService.Login();
 
-        Console.WriteLine("Password:");
-        var password = Console.ReadLine();
-        var staff = AppDb.StaffTable.Where(_ => _.Value.StaffId == staffId && _.Value.Password == password).SingleOrDefault().Value;
-        if(staff is null)
-        {
-            Console.WriteLine("Invalid Credential");
-            return;
-        }
 
-        Console.WriteLine($"Welcome {staff.FirstName} {staff.LastName}");
+
+
+        Console.WriteLine($"Welcome {loginResult?.FirstName} {loginResult?.LastName}");
         Console.WriteLine($"Select a system menu from the list below");
 
         int menuCounter = 0;
-        if(staff.Designation == "System Admin")
+        if(loginResult?.Designation == "System Admin")
         {
             List<string> systemMenu = new List<string> { "Create a new Staff", "View Staff", "Edit Staff", "Setup a new Restaurant", "Setup Restaurant Menu" };
             foreach (var sysMenu in systemMenu)
---
+++ Restaurant.Application/Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using Restaurant.Domain.Db;
+using Restaurant.Domain.Entities;
+
+namespace Restaurant.Application.Services;
+public class AuthService
+{
+    public Task<Staff> Login()
+    {
+        Console.WriteLine("Enter your login details");
+        Console.WriteLine("Staff Id:");
+        var staffId = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("Password:");
+        var password = Console.ReadLine();
+        var staff = AppDb.StaffTable.Where(_ => _.Value.StaffId == staffId && _.Value.Password == password).SingleOrDefault().Value;
+        if (staff is null)
+        {
+            Console.WriteLine("Invalid login details");
+            Login();
+        }
+        return Task.FromResult(staff!);
+    }
+}
