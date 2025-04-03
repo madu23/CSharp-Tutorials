c412c0f9adc9399f3d2ef167cbdb67d69244da3d
Adejorooluwatobi <Adejorotgold1@gmail.com>
2025-04-03 11:53:21 +0100

    AdminPortal

--- AdminPortal/login.js
+++ AdminPortal/login.js
@@ -1,8 +1,8 @@
 function validateLogin(event) {
     event.preventDefault(); 
     
-    var username = document.getElementById('username').value.trim();
-    var password = document.getElementById('password').value.trim();
+    let username = document.getElementById('username').value.trim();
+    let password = document.getElementById('password').value.trim();
     
     if (username === 'superAdmin' && password === 'P@ssw0rd#') {
         window.location.href = 'dashboard.html'; 
